1e40555fc5ec35575d5b92d728565b27067a8763
Tomek1 <Tomek1@Tomek>
2016-06-27 12:53:19 +0200

    new

--- AngularSpringdemo2/src/main/java/com/example/configuration/CsrfHeaderFilter.java
+++ AngularSpringdemo2/src/main/java/com/example/configuration/CsrfHeaderFilter.java
@@ -4,13 +4,10 @@ import java.io.IOException;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.filter.OncePerRequestFilter;
-import org.springframework.web.util.WebUtils;
 
 /**
  * @author Tomek1
@@ -25,17 +22,17 @@ public class CsrfHeaderFilter extends OncePerRequestFilter {
 	protected void doFilterInternal(HttpServletRequest request,
 			HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
-		CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
-		        .getName());
-		    if (csrf != null) {
-		      Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
-		      String token = csrf.getToken();
-		      if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
-		        cookie = new Cookie("XSRF-TOKEN", token);
-		        cookie.setPath("/");
-		        response.addCookie(cookie);
-		      }
-		    }
+//		CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class
+//		        .getName());
+//		    if (csrf != null) {
+//		      Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+//		      String token = csrf.getToken();
+//		      if (cookie==null || token!=null && !token.equals(cookie.getValue())) {
+//		        cookie = new Cookie("XSRF-TOKEN", token);
+//		        cookie.setPath("/");
+//		        response.addCookie(cookie);
+//		      }
+//		    }
 		    filterChain.doFilter(request, response);
 
 	}
